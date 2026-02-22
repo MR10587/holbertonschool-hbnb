fe881827e207acd95a28547055bfbdb3fdcf61cf
MR10587 <ramalmirzezade20@gmail.com>
2026-02-22 15:18:49 +0400

    .

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -5,7 +5,9 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
+jwt = JWTManager()
 
 bcrypt = Bcrypt()
 
@@ -14,6 +16,7 @@ def create_app(config_class = "config.DevelopmentConfig"):
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
